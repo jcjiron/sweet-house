69d4b905632a4645d7a5d38dc1c732102d3c0718
Juan Carlos Jiron <jc.jiron@casai.com>
2020-09-10 21:46:52 -0500

    fix: resolved conflicts

--- src/app/features/new-suite/resume/resume.component.ts
+++ src/app/features/new-suite/resume/resume.component.ts
@@ -2,13 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AppState } from 'src/app/app.reducer';
 import { Store } from '@ngrx/store';
 import { SuiteInterface } from 'src/app/shared/models/suite.interface';
-<<<<<<< HEAD
 import { HttpRequestService } from 'src/app/shared/services/http-request.service';
 import { Router } from '@angular/router';
 import { NEW_SUITE_POST } from 'src/app/core/environment/app-urls.constant';
-=======
-import { Router } from '@angular/router';
->>>>>>> 1b9283e7c45ce7ca07e6aa3abfaea82e8c5d0b4a
 
 @Component({
   selector: 'app-resume',
@@ -17,28 +13,28 @@ import { Router } from '@angular/router';
 })
 export class ResumeComponent implements OnInit {
 
-  public newsuiteInfo:SuiteInterface;
+  public newsuiteInfo: SuiteInterface;
 
   constructor(
-    private store:Store<AppState>,
-    private http:HttpRequestService,
-    private router:Router
+    private store: Store<AppState>,
+    private http: HttpRequestService,
+    private router: Router
   ) { }
 
   ngOnInit() {
 
     this.store.select('newSuite')
-      .subscribe(newSuite=>{
+      .subscribe(newSuite => {
 
         this.newsuiteInfo = newSuite;
       });
   }
 
 
-  postSuite(){
+  postSuite() {
     this.http.post(NEW_SUITE_POST, this.newsuiteInfo)
-      .subscribe(data=>{
-        
+      .subscribe(data => {
+
         console.log(data);
         this.router.navigate(['my-suites']);
       })
